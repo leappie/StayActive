887d9433c189a22b261548e0efa3ad8c4b606352
leappie <50877036+leappie@users.noreply.github.com>
2022-05-15 15:57:32 +0200

    changes

---
+++ Infrastructure/src/test/java/persistence/DAOTest.java
@@ -0,0 +1,27 @@
+package persistence;
+
+import org.dbunit.DataSourceBasedDBTestCase;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.sqlite.SQLiteDataSource;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+
+public abstract class DAOTest extends DataSourceBasedDBTestCase {
+
+    @Override
+    protected DataSource getDataSource() {
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl("jdbc:sqlite:src/test/resources/stayactive_testdb.db");
+        return dataSource;
+    }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(new FileInputStream(getTestData()));
+    }
+
+    protected abstract String getTestData();
+}
+
--- Infrastructure/src/test/java/persistence/user/UserDAOTest.java
+++ Infrastructure/src/test/java/persistence/user/UserDAOTest.java
@@ -1,49 +1,73 @@
 package persistence.user;
 
 import entity.User;
+import enums.Level;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import persistence.DAOTest;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 
 class UserDAOTest extends DAOTest {
 
     private UserDAO userDAO = new UserDAO(getDataSource());
-
+    private String testData = "src/test/resources/user_testdata.xml";
 
     @Test
     void testInsert() throws Exception {
+        /*
+        INSERT
+         */
         //Arrange
-        IDataSet expectedDataSet = getDataSet();
-        ITable expectedTable = expectedDataSet.getTable("user");
+        DatabaseOperation.DELETE_ALL.execute(getConnection(), getDataSet());
         IDataSet databaseDataSet = getConnection().createDataSet();
-        ITable actualTable = databaseDataSet.getTable("user");
 
-
-        // Act
-        getSetUpOperation();
-        getTearDownOperation();
-        DatabaseOperation.CLEAN_INSERT.execute(getConnection(), getDataSet());
-        this.userDAO.insert(new User("testt", "1234"));
+        // Act;
+        long idInsert1 = this.userDAO.insert(new User("test1", "1234"));
+        long idInsert2 = this.userDAO.insert(new User("test2", "1234"));
+        long idInsert3 = this.userDAO.insert(new User("test3", "1234"));
 
         //Assert
-        assertEquals(expectedTable.getRowCount(), actualTable.getRowCount());
+        ITable actualTable = databaseDataSet.getTable("user");
+        assertEquals(3, actualTable.getRowCount());
     }
-//
-//    @Test
-//    void update() {
-//    }
-//
-//    @Test
-//    void delete() {
-//    }
 
     @Test
-    void testQuery() throws Exception {
+    void update() throws Exception {
+        //Arrange
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        User user = getUser(new User("test1", "1234"));
 
+        // Act;
+        this.userDAO.update(new User(user.getId(), "updatedUser", "updatedPassword", Level.HARD));
+
+
+        //Assert
+        ITable actualTable = databaseDataSet.getTable("user");
+//        actualTable.
+//        assertEquals(3, actualTable.getRowCount());
 
     }
 
+    @Test
+    void delete() {
+    }
 
 
+    private User getUser(User queryUser) throws Exception {
+        User user = this.userDAO.query(queryUser).get(0);
+        return user;
+    }
+
+    @Override
+    protected String getTestData() {
+        return testData;
+    }
 }
