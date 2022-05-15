d2e19c019b04cba9daee4382771a8cfa389039fd
leappie <50877036+leappie@users.noreply.github.com>
2022-05-15 21:44:52 +0200

    changes

--- Infrastructure/src/test/java/persistence/user/UserDAOTest.java
+++ Infrastructure/src/test/java/persistence/user/UserDAOTest.java
@@ -5,15 +5,11 @@ import enums.Level;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
 import org.dbunit.operation.DatabaseOperation;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import persistence.DAOTest;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
+import java.util.List;
 
 class UserDAOTest extends DAOTest {
 
@@ -21,7 +17,7 @@ class UserDAOTest extends DAOTest {
     private String testData = "src/test/resources/user_testdata.xml";
 
     @Test
-    void testInsert() throws Exception {
+    void testCRUD() throws Exception {
         /*
         INSERT
          */
@@ -32,38 +28,51 @@ class UserDAOTest extends DAOTest {
         // Act;
         long idInsert1 = this.userDAO.insert(new User("test1", "1234"));
         long idInsert2 = this.userDAO.insert(new User("test2", "1234"));
-        long idInsert3 = this.userDAO.insert(new User("test3", "1234"));
 
         //Assert
         ITable actualTable = databaseDataSet.getTable("user");
-        assertEquals(3, actualTable.getRowCount());
-    }
+        Assertions.assertEquals(2, actualTable.getRowCount());
 
-    @Test
-    void update() throws Exception {
+        /*
+        UPDATE
+         */
         //Arrange
-        IDataSet databaseDataSet = getConnection().createDataSet();
-        User user = getUser(new User("test1", "1234"));
-
         // Act;
-        this.userDAO.update(new User(user.getId(), "updatedUser", "updatedPassword", Level.HARD));
-
+        this.userDAO.update(new User((int) idInsert1, "updatedUser1", "updatedPassword1", Level.HARD));
+        this.userDAO.update(new User((int) idInsert2, "updatedUser2", "updatedPassword2", Level.HARD));
 
         //Assert
-        ITable actualTable = databaseDataSet.getTable("user");
-//        actualTable.
-//        assertEquals(3, actualTable.getRowCount());
+        String username1 = (String) databaseDataSet.getTable("user").getValue(0, "username"); // get row 0, column username
+        String username2 = (String) databaseDataSet.getTable("user").getValue(1, "username"); // get row 1, column username
+        String password1 = (String) databaseDataSet.getTable("user").getValue(0, "password"); // get row 0, column password
+        String password2 = (String) databaseDataSet.getTable("user").getValue(1, "password"); // get row 1, column password
+        Assertions.assertEquals(username1, "updatedUser1");
+        Assertions.assertEquals(password1, "updatedPassword1");
+        Assertions.assertEquals(username2, "updatedUser2");
+        Assertions.assertEquals(password2, "updatedPassword2");
 
-    }
+        /*
+        DELETE
+         */
+        //Arrange
+        // Act;
+        this.userDAO.delete(new User("updatedUser1", "updatedPassword1"));
 
-    @Test
-    void delete() {
-    }
+        //Assert
+        actualTable = databaseDataSet.getTable("user");
+        Assertions.assertEquals(1, actualTable.getRowCount());
 
 
-    private User getUser(User queryUser) throws Exception {
-        User user = this.userDAO.query(queryUser).get(0);
-        return user;
+        /*
+        Query
+         */
+        //Arrange
+        // Act;
+        List<User> userList = this.userDAO.query(new User("updatedUser2", "updatedPassword2"));
+
+        //Assert
+        Assertions.assertEquals(1, userList.size());
+        Assertions.assertEquals("updatedUser2", userList.get(0).getUsername());
     }
 
     @Override
